6e2d0c8152ea60e647c2d70ea5e1da0f066eee88
Charles J McDonald <cjmcdonald42@gmail.com>
2024-10-07 11:19:57 -0400

    Create outline.py

---
+++ outline.py
@@ -0,0 +1,29 @@
+# Introduce the Game
+# Rules = Rock, Paper, or Scissors
+
+# Get the names of player 1 and player 2
+# Player 1 chooses [R]ock, [S]cissors, [P]aper
+# Player 2 chooses [R]ock, [S]cissors, [P]aper
+
+# if player1 == Rock
+#   if player2 == Rock
+#       --> Draw
+#   elif player2 == Paper
+#       --> p2 win
+#   elif player2 == Scissors
+#       -- p2 lose
+# elif player1 == Paper
+#   if player2 == Rock
+#       --> P2 lose
+#   elif player2 == Paper
+#       --> Draw
+#   elif player2 == Scissors
+#       -- p2 win
+# elif player1 == Scissors
+#   if player2 == Rock
+#       --> P2 win
+#   elif player2 == Paper
+#       --> p2 lose
+#   elif player2 == Scissors
+#       -- draw
+#hi
