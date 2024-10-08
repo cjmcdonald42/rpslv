9fcea01d0dd3a8c786d6c78fdae054187744ea1d
Charles J McDonald <cjmcdonald42@gmail.com>
2024-10-08 14:06:09 -0400

    Add more logic for 2 players

--- rpslv.py
+++ rpslv.py
@@ -1,10 +1,9 @@
 #
 #           project:    rpslv.py
 #            author:    Charles J McDonald <cmcdonald@woonsocketschools.com>
-#           version:    2.0
-#   last updated on:    10/8/2024
+#           version:    2024.10.8.02
 #
-#   description: Rock, Paper, Scissors, Lizard, Spock for 2 players
+#   description: Rock, Paper, Scissors, Lizard, Spock for 1 or 2 players
 #
 
 # === Introduce the Game
@@ -16,7 +15,9 @@ print("""
 
 # === Get the names of our challengers
 player1 = input("First Challenger, what is thy name? : ")
-player2 = input("Second Challenger, what is thy name? : ")
+player2 = input("Second Challenger, state thy name or enter 'C' to practice against the computer: ")
+weaponsFullName = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+weapons = ["R", "P", "S", "L", "V"]
 score1 = score2 = 0
 isPlaying = True
 
@@ -32,11 +33,16 @@ while isPlaying:
     """)
 
     weapon1 = input(f"{player1}, choose thy weapon! : ")
-    weapon2 = input(f"{player2}, choose thy weapon! : ")
+    if player2 == "C":  # Computer randomly choses a weapon
+        randomWeapon = random.randint(0, 4)
+        weapon2 = weapons[randomWeapon]
+        print(f"Horatio the Chaotic Computer chooses {weaponsFullName[randomWeapon]}")
+    else:
+        weapon2 = input(f"{player2}, choose thy weapon! : ")
 
     # === Resolve combat
-    if weapon1 == "R" :     #   Player1 chooses Rock
-        if weapon2 == "R" :
+    if weapon1 == "R":  # Player1 chooses Rock
+        if weapon2 == "R":
             print("Thou hast ended in a Draw!")
         elif weapon2 == "P":
             print(f"Paper covers Rock! {player2} wins this round!")
@@ -47,27 +53,27 @@ while isPlaying:
         elif weapon2 == "L":
             print(f"Rock crushes Lizard! {player1} wins this round!")
             score1 = score1 + 1
-        else :      # weapon2 must be Scissors
+        else:  # weapon2 must be Scissors
             print(f"Rock crushes Scissors! {player1} wins this round!")
             score1 = score1 + 1
-            
-    elif weapon1 == "P" :    #   Player1 chooses Paper
-        if weapon2 == "P" :
+
+    elif weapon1 == "P":  # Player1 chooses Paper
+        if weapon2 == "P":
             print("Thou hast ended in a Draw!")
-        elif weapon2 == "R" :
+        elif weapon2 == "R":
             print(f"Paper covers Rock! {player1} wins this round!")
             score1 = score1 + 1
-        elif weapon2 == "V" :
+        elif weapon2 == "V":
             print(f"Paper disprooves Spock! {player1} wins this round!")
             score1 = score1 + 1
         elif weapon2 == "L":
             print(f"Lizard shreds Paper! {player2} wins this round!")
             score2 = score2 + 1
-        else :
+        else:
             print(f"Scissors cut Paper! {player2} wins this round!")
             score2 = score2 + 1
-            
-    elif weapon1 == "L":    #   Player1 chooses Lizard
+
+    elif weapon1 == "L":  # Player1 chooses Lizard
         if weapon2 == "L":
             print("Thou hast ended in a Draw!")
         elif weapon2 == "V":
@@ -82,8 +88,8 @@ while isPlaying:
         else:
             print(f"Scissors decapitate Lizard! {player2} wins this round!")
             score2 = score2 + 1
-            
-    elif weapon1 == "V":    #   Player1 chooses Spock
+
+    elif weapon1 == "V":  # Player1 chooses Spock
         if weapon2 == "V":
             print("Thou hast ended in a Draw!")
         elif weapon2 == "S":
@@ -99,7 +105,7 @@ while isPlaying:
             print(f"Paper disproves Spock! {player2} wins this round!")
             score2 = score2 + 1
 
-    else:           # weapon1 must be Scissors
+    else:  # weapon1 must be Scissors
         if weapon2 == "S":
             print("Thou hast ended in a Draw!")
         elif weapon2 == "L":
@@ -120,13 +126,15 @@ while isPlaying:
     {player2} hath scored {score2} points.
     """)
 
-    if score1 > score2:
-        print(f"{player1} wins the duel!")
-    elif score2 > score1:
-        print(f"{player2} wins the duel!")
-    else:
-        print("The duel ends in a draw!")
-
     # == Play again?
     ask = input("Would thoust like another round? : ")
-    if ask != "Y": isPlaying = False
+    if ask != "Y" :
+        isPlaying = False
+
+if score1 > score2:
+    print(f"{player1} win-ith the duel with {score1} points!")
+elif score2 > score1:
+    print(f"{player2} win-ith the duel with {score2} points!")
+else:
+    print("The duel doth end in a draw!")
+
