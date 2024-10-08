782edb743c3ea84467008b8a565c4b77ae18bc3e
Charles J McDonald <cjmcdonald42@gmail.com>
2024-10-08 10:56:45 -0400

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,10 +1,11 @@
 #
 #   project:    rps.py
 #   author:     Charles J McDonald <cmcdonald@woonsocketschools.com>
+#   version:    0.9
+#   maturity:   alpha
 #
 #   description: Rock, Paper, Scissors for 2 players
 #
-# import getpass      # Password method will allow hidden input
 
 # === Introduce the Game
 # Rules = Rock, Paper, or Scissors
@@ -17,35 +18,61 @@ print("""
 player1 = input("First Challenger, what is thy name? : ")
 player2 = input("Second Challenger, what is thy name? : ")
 score1 = score2 = 0
+isPlaying = True
 
-# === Choose your move
-print("""
-    On yon table before thee reste weapons three :
-        [R]ock : An ancient weapon of destruction
-        [P]aper: An elegant weapon for the literate
-        [S]cissors: A brutish blade for the stylish
-""")
+while isPlaying:
+    # === Choose your move
+    print("""
+        On yon table before thee reste weapons three :
+            [R]ock : An ancient weapon of destruction
+            [P]aper: An elegant weapon for the literate
+            [S]cissors: A brutish blade for the stylish
+    """)
 
-weapon1 = input(f"{player1}, choose thy weapon! : ").upper()      # make uppercase
-weapon2 = input(f"{player2}, choose thy weapon! : ").upper()
+    weapon1 = input(f"{player1}, choose thy weapon! : ")
+    weapon2 = input(f"{player2}, choose thy weapon! : ")
 
-# === Resolve combat
-if weapon1 == "R" :
-    if weapon2 == "R" :
-        print("Thou hast ended in a Draw!")
-    elif weapon2 == "P" :
-        print(f"Paper covers Rock! {player2} wins this round!")
-        score2 = score2 + 1
-    else :
-        print(f"Rock crushes Scissors! {player1} wins this round!")
-        score1 = score1 + 1
+    # === Resolve combat
+    if weapon1 == "R" :
+        if weapon2 == "R" :
+            print("Thou hast ended in a Draw!")
+        elif weapon2 == "P" :
+            print(f"Paper covers Rock! {player2} wins this round!")
+            score2 = score2 + 1
+        else :
+            print(f"Rock crushes Scissors! {player1} wins this round!")
+            score1 = score1 + 1
+    elif weapon1 == "P" :
+        if weapon2 == "P" :
+            print("Thou hast ended in a Draw!")
+        elif weapon2 == "R" :
+            print(f"Paper covers Rock! {player1} wins this round!")
+            score1 = score1 + 1
+        else :
+            print(f"Scissors cut Paper! {player2} wins this round!")
+            score2 = score2 + 1
+    else:   # weapon1 must be Scissors
+        if weapon2 == "S" :
+            print("Thou hast ended in a Draw!")
+        elif weapon2 == "R" :
+            print(f"Rock breaks Scissors! {player2} wins this round!")
+            score2 = score2 + 1
+        else :
+            print(f"Scissors cut Paper! {player1} wins this round!")
+            score1 = score1 + 1
 
-print(f"""
-{player1} hath scored {score1} points. \n
-{player2} hath scored {score2} points. \n
-""")
+    print(f"""
+    {player1} hath scored {score1} points. \n
+    {player2} hath scored {score2} points. \n
+    """)
 
-if score1 > score2:
-    print(f"{player1} wins the duel!")
-else:
-    print(f"{player2} wins the duel!")
+    if score1 > score2:
+        print(f"{player1} wins the duel!")
+    elif score2 > score1:
+        print(f"{player2} wins the duel!")
+    else:
+        print("The duel ends in a draw!")
+
+    # == Play again?
+    ask = input("Would you like another round? : ")
+    if ask == "N": isPlaying = False
